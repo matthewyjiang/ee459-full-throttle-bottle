c9095829fd2358167684d5aa8e49a28764d1f3f6
Matthew Jiang <jiangmy@usc.edu>
2025-04-20 17:53:14 -0700

    added adc and i2c

--- att/main.c
+++ att/main.c
@@ -1,21 +1,109 @@
 #include <avr/io.h>
-#include <util/delay.h>
-// #include "i2c.h"
+#include <avr/interrupt.h>
+#include <util/delay.h> 
+#include "i2c_secondary.c"
 
 #define FOSC 8000000 // Clock Speed
 #define BDIV (FOSC / 100000 - 16) / 2 + 1
+#define DEVICE_ADDRESS 0x50 // I2C Slave Address
+
+// Define your I2C slave address (7-bit)
+#define MY_I2C_ADDRESS 0x42
+
+#define READ_BUFFER_SIZE 5 // Size of the read buffer
+#define ADC_CHANNEL 3     // Using ADC0 (can be 0-7 for most AVRs)
+
+unsigned char read_buffer[READ_BUFFER_SIZE]; // Buffer to store read data
+
+// Function to initialize ADC
+void adc_init(void) {
+    // Set ADC reference voltage to AVCC
+    ADMUX = (1 << REFS0);
+    
+    // Select ADC channel
+    ADMUX = (ADMUX & 0xF8) | (ADC_CHANNEL & 0x07);
+    
+    // Enable ADC and set prescaler to 64 (typical value for 8MHz)
+    // This gives an ADC clock of 125kHz which is in the recommended range
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1);
+}
+
+// Function to read from ADC
+uint16_t adc_read(void) {
+    // Start conversion
+    ADCSRA |= (1 << ADSC);
+    
+    // Wait for conversion to complete
+    while (ADCSRA & (1 << ADSC));
+    
+    // Return ADC value (10-bit)
+    return ADC;
+}
+
+// Function to initialize PWM on PB4
+void pwm_init(void) {
+    // Set PB4 as output
+    DDRB |= (1 << PB4);
+    
+    // Clear output pin initially
+    PORTB &= ~(1 << PB4);
+}
+
+// Better software PWM implementation using PB4
+void better_software_pwm(uint8_t duty_cycle) {
+    static uint8_t pwm_counter = 0;
+    
+    // Increment the counter
+    pwm_counter++;
+    
+    // Compare counter with duty cycle
+    if (pwm_counter < duty_cycle) {
+        PORTB |= (1 << PB4);  // Set PB4 high
+    } else {
+        PORTB &= ~(1 << PB4); // Set PB4 low
+    }
+}
+
 
 int main(void) {
-
-    // i2c_init(BDIV); 
-
-    DDRB |= (1 << PB4); 
-    
-    while (1) {
-        PORTB |= (1 << PB4); // Set PB4 high
-        _delay_ms(1000); // Wait for 1 second
-        PORTB &= ~(1 << PB4); // Set PB4 low
-        _delay_ms(1000); // Wait for 1 second
+    // Initialize buffers
+    for (int i = 0; i < READ_BUFFER_SIZE; i++) {
+        read_buffer[i] = 0x00; // Initialize the read buffer
     }
 
+    // Initialize I2C secondary
+    i2c_secondary_init(MY_I2C_ADDRESS, read_buffer, READ_BUFFER_SIZE);
+    
+    // Initialize ADC
+    adc_init();
+    
+    // Initialize PWM on PB4
+    pwm_init();
+    
+    // Enable global interrupts
+    sei();
+    
+    uint8_t pwm_duty = 128; // Default duty cycle value
+    
+    while (1) {
+        // Read ADC value
+        uint16_t adc_value = adc_read();
+        
+        // Store ADC value in read buffer
+        read_buffer[0] = (adc_value >> 8) & 0xFF;  // High byte
+        read_buffer[1] = adc_value & 0xFF;         // Low byte
+        
+        
+        pwm_duty = 0x00;
+        
+        // Perform one step of the PWM cycle
+        better_software_pwm(pwm_duty);
+        
+        // Small delay to control PWM frequency
+        // Adjust this value to get desired frequency
+        _delay_us(10);
+        
+        // Other tasks can be performed here without
+        // blocking the entire PWM cycle
+    }
 }
--- esp/src/main.c
+++ esp/src/main.c
@@ -1,15 +1,102 @@
 #include "esp_common.h"
 #include "freertos/task.h"
 #include "gpio.h"
+#include "i2c_master.h" // Include the ESP8266 I2C master driver
 
-// PWM configuration
-#define PWM_GPIO 16     // GPIO pin for PWM output
-#define PWM_PERIOD 100  // PWM period in timer ticks 
-#define PWM_RESOLUTION 100 // PWM resolution (0-100)
+// Defines for I2C operations
+#define MAX_I2C_DEVICES 10    // Maximum number of I2C devices to scan for
+#define I2C_SCAN_INTERVAL_MS 500  // Scan I2C bus every 5 seconds
+#define I2C_WRITE_VALUE 0x55 // Example value to write to devices
 
 // Global PWM variables
-static uint8_t pwm_duty = 90;  // Initial duty cycle (50%)
+static uint8_t pwm_duty = 90;  // Initial duty cycle (90%)
 static bool pwm_direction = true; // For breathing effect
+
+// Array to store found I2C device addresses
+static uint8_t i2c_devices[MAX_I2C_DEVICES];
+static uint8_t i2c_device_count = 0;
+
+// Function prototypes for I2C operations
+void i2c_scan_task(void* pvParameters);
+void i2c_read_write_task(void* pvParameters);
+bool i2c_scan_bus(void);
+
+/**
+ * @brief Write data to an I2C device
+ * 
+ * @param device_addr Device address (7-bit)
+ * @param data Pointer to data buffer
+ * @param data_len Length of data to write
+ * @return bool True if successful, false otherwise
+ */
+bool i2c_write_data(uint8_t device_addr, uint8_t* data, uint8_t data_len)
+{
+    uint8_t i;
+    
+    // Start condition
+    i2c_master_start();
+    
+    // Send device address with write bit
+    i2c_master_writeByte(device_addr << 1);
+    if (!i2c_master_checkAck()) {
+        i2c_master_stop();
+        return false;
+    }
+    
+    // Send data bytes
+    for (i = 0; i < data_len; i++) {
+        i2c_master_writeByte(data[i]);
+        if (!i2c_master_checkAck()) {
+            i2c_master_stop();
+            return false;
+        }
+    }
+    
+    // Stop condition
+    i2c_master_stop();
+    return true;
+}
+
+/**
+ * @brief Read data from an I2C device
+ * 
+ * @param device_addr Device address (7-bit)
+ * @param data Pointer to data buffer to store read data
+ * @param data_len Length of data to read
+ * @return bool True if successful, false otherwise
+ */
+bool i2c_read_data(uint8_t device_addr, uint8_t* data, uint8_t data_len)
+{
+    uint8_t i;
+    
+    // Start condition
+    i2c_master_start();
+    
+    // Send device address with read bit
+    i2c_master_writeByte((device_addr << 1) | 0x01);
+    if (!i2c_master_checkAck()) {
+        i2c_master_stop();
+        return false;
+    }
+    
+    // Read data bytes
+    for (i = 0; i < data_len; i++) {
+        data[i] = i2c_master_readByte();
+        
+        // Send ACK for all bytes except the last one
+        if (i < data_len - 1) {
+            i2c_master_send_ack();
+        } else {
+            i2c_master_send_nack();  // Last byte gets NACK
+        }
+    }
+    
+    // Stop condition
+    i2c_master_stop();
+    return true;
+}
+
+
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -49,33 +136,102 @@ uint32 user_rf_cal_sector_set(void)
     return rf_cal_sec;
 }
 
-// PWM software implementation task
-void task_pwm(void* ignore)
+
+// I2C bus scanner task
+bool i2c_scan_bus(void)
 {
-    // Configure GPIO16 as output
-    gpio16_output_conf();
+    uint8_t i;
+    uint8_t found_count = 0;
+    bool ack;
     
-    uint8_t tick_count = 0;
+    // Clear previous scan results
+    for (i = 0; i < MAX_I2C_DEVICES; i++) {
+        i2c_devices[i] = 0;
+    }
     
-    while(true) {
-        // One PWM cycle
-        for (tick_count = 0; tick_count < PWM_RESOLUTION; tick_count++) {
-            // Set GPIO high if current tick is less than duty cycle
-            if (tick_count < pwm_duty) {
-                gpio16_output_set(1);
+    // Scan all possible addresses (0x01-0x7F)
+    for (i = 1; i < 128; i++) {
+        i2c_master_start();
+        i2c_master_writeByte(i << 1);
+        i2c_master_stop();
+        
+        if (ack) {
+            // Store the device address if we have space
+            if (found_count < MAX_I2C_DEVICES) {
+                i2c_devices[found_count] = i;
+                found_count++;
             } else {
-                gpio16_output_set(0);
+                break;
             }
-            
-            // Short delay to control PWM frequency
-            // Adjust this value to get desired PWM frequency
-            os_delay_us(100); 
         }
+        
+        // Small delay between scans
+        os_delay_us(1000);
+    }
+    
+    // Update global device count
+    i2c_device_count = found_count;
+    
+    return (found_count > 0);
+}
+
+// I2C bus scanner task
+void i2c_scan_task(void* pvParameters)
+{
+    while (true) {
+        // Scan the I2C bus
+        i2c_scan_bus();
+        
+        // Wait before next scan
+        vTaskDelay(I2C_SCAN_INTERVAL_MS / portTICK_RATE_MS);
     }
     
     vTaskDelete(NULL);
 }
 
+// I2C read/write task for multiple devices
+void i2c_read_write_task(void* pvParameters)
+{
+    uint8_t i;
+    uint8_t data;
+    
+    while (true) {
+        // Only proceed if we have devices
+        if (i2c_device_count > 0) {
+            // Loop through all found devices
+            for (i = 0; i < i2c_device_count; i++) {
+                uint8_t device_addr = i2c_devices[i];
+                
+                // Skip if the address is 0 (invalid)
+                if (device_addr == 0) {
+                    continue;
+                }
+
+                uint8_t* send_buffer;
+                send_buffer = (uint8_t*)malloc(1);
+                if (send_buffer == NULL) {
+                    // Handle memory allocation failure
+                    continue;
+                }
+                send_buffer[0] = 0x41; 
+
+                
+                i2c_write_data(device_addr, send_buffer, 1); // Write example value
+                
+                free(send_buffer); 
+                
+                
+                // Small delay between devices
+                vTaskDelay(100 / portTICK_RATE_MS);
+            }
+        }
+        
+        // Wait before next read/write cycle
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+    
+    vTaskDelete(NULL);
+}
 
 /******************************************************************************
  * FunctionName : user_init
@@ -85,7 +241,15 @@ void task_pwm(void* ignore)
 *******************************************************************************/
 void user_init(void)
 {
-    // Create PWM task with higher priority
-    xTaskCreate(&task_pwm, "pwm_task", 2048, NULL, 3, NULL);
     
+    // Initialize I2C master
+    i2c_master_gpio_init();
+    i2c_master_init();
+    
+    
+    // Create I2C scanner task with lower priority
+    xTaskCreate(&i2c_scan_task, "i2c_scan", 2048, NULL, 1, NULL);
+    
+    // Create I2C read/write task with medium priority
+    xTaskCreate(&i2c_read_write_task, "i2c_rw", 2048, NULL, 2, NULL);
 }
